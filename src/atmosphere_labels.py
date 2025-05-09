7d2f510aff00ca12394eefad833b956a65e3edbf
Karen Stuitje <e.k.e.stuitje@student.rug.nl>
2025-05-09 13:16:38 +0200

    Add new atmospheres

--- src/atmosphere_labels.py
+++ src/atmosphere_labels.py
@@ -2,16 +2,18 @@ atmosphere_labels = {
     "bare_rock": "Bare rock",
     "earth_like": "1 bar 79% N$_2$ + 21% O$_2$ + 400 ppm CO$_2$",
 
-    # 0.1 bar entries
+    # 0.1 bar 
     "01bar_CO2": "0.1 bar CO$_2$",
+    "01bar_SO2": "0.1 bar SO$_2$",
     "01bar_H2O": "0.1 bar H$_2$O",
     "01bar_N2": "0.1 bar N$_2$",
     "01bar_O2": "0.1 bar O$_2$",
     "01bar_O2_H2O": "0.1 bar O$_2$ + 1% H$_2$O",
     "01bar_N2_1000ppm_CO2": "0.1 bar N$_2$ + 1000 ppm CO$_2$",
     "01bar_N2_1000ppm_H2O": "0.1 bar N$_2$ + 1000 ppm H$_2O$",
+    "01bar_N2_1000ppm_SO2": "0.1 bar N$_2$ + 1000 ppm SO$_2$",
 
-    # 1 bar entries
+    # 1 bar 
     "1bar_CO2": "1 bar CO$_2$",
     "1bar_H2O": "1 bar H$_2$O",
     "1bar_N2": "1 bar N$_2$",
@@ -23,7 +25,7 @@ atmosphere_labels = {
     "1bar_O2": "1 bar O$_2$",
     "1bar_O2_100ppm_H2O": "1 bar O$_2$ + 100 ppm H$_2$O",
 
-    # 10 bar entries
+    # 10 bar 
     "10bar_CO2": "10 bar CO$_2$",
     "10bar_N2": "10 bar N$_2$",
     "10bar_N2_1ppm_CO2": "10 bar N$_2$ + 1 ppm CO$_2$",
