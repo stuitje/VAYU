c23be1dd975f30aba5b21a117c8a2d7fd7401f29
Karen Stuitje <e.k.e.stuitje@student.rug.nl>
2025-05-15 00:28:47 +0200

    Add constants

--- src/constants.py
+++ src/constants.py
@@ -1,15 +1,21 @@
-# src/constants.py
-
-# physical constants (SI units)
-planck_const = 6.62607015e-34         # Planck constant (J·s)
-boltzmann = 1.380649e-23              # Boltzmann constant (J/K)
-speed_of_light = 2.99792458e8         # Speed of light in vacuum (m/s)
-stefan_boltzmann = 5.670374419e-8     # Stefan-Boltzmann constant [W/m²/K⁴]
+# physical constants
+planck_const       = 6.62607015e-34   # Planck constant (J·s)
+boltzmann          = 1.380649e-23     # Boltzmann constant (J/K)
+speed_of_light     = 2.99792458e8     # Speed of light (m/s)
+stefan_boltzmann   = 5.670374419e-8   # Stefan–Boltzmann constant (W/m2/K4)
+elementary_charge  = 1.602176634e-19  # Elementary charge (C)
+avogadro           = 6.02214076e23    # Avogadro's number (1/mol)
 
 # astronomical constants
-r_earth = 6.371e6               # Earth radius (m)
-m_earth = 5.972e24              # Earth mass (kg)
-r_sun = 6.957e8                 # Solar radius (m)
-l_sun = 3.828e26                # Solar luminosity (W)
-au = 1.496e11                   # Astronomical unit (m)
-G = 6.67430e-11                 # Gravitational constant (m³/kg/s²)
+
+r_earth   = 6.371e6           # Earth radius (m)
+m_earth   = 5.972e24          # Earth mass (kg)
+r_sun     = 6.957e8           # Solar radius (m)
+m_sun     = 1.989e30          # Solar mass (kg)
+l_sun     = 3.828e26          # Solar luminosity (W)
+au        = 1.496e11          # Astronomical unit (m)
+pc        = 3.085677581e16    # Parsec (m)
+G         = 6.67430e-11       # Gravitational constant (m3/kg/s2)
+
+# jwst
+jwst_collecting_area =  25.0   # JWST effective area (m2)
