5fd457d1d394870bd0a390fc51f5706d61e66502
Karen Stuitje <e.k.e.stuitje@student.rug.nl>
2025-05-19 18:11:43 +0200

    remove agni dataset loader (now in dataloader.py)

--- src/utils.py
+++ src/utils.py
@@ -57,34 +57,6 @@ def contrast_ppm(
     contrast = (planet_flux / star_flux) * radius_ratio_sq * 1e6  # Convert to ppm
     return contrast
 
-def load_agni_output(nc_path: str) -> dict:
-    """
-    Load AGNI NetCDF output and return key data arrays.
-
-    Args:
-        nc_path: Path to AGNI .nc file
-
-    Returns:
-        Dictionary with bandcenter (nm), longwave and shortwave fluxes,
-        and total flux. 
-    """
-    ds = nc.Dataset(nc_path)
-    bandmin = ds["bandmin"][:]
-    bandmax = ds["bandmax"][:]
-    bandcenter = (bandmin + bandmax) / 2 * 1e9  # convert to nm
-
-    bandwidth = (bandmax - bandmin) * 1e9  # nm
-    flux_lw = ds["ba_U_LW"][1, :] / bandwidth
-    flux_sw = ds["ba_U_SW"][1, :] / bandwidth
-    flux_total = flux_lw + flux_sw
-
-    return {
-        "bandcenter": bandcenter,
-        "ba_U_LW": flux_lw,
-        "ba_U_SW": flux_sw,
-        "ba_U_total": flux_total
-    }
-
 def compute_equilibrium_temperature(
     stellar_luminosity: float,
     distance_au: float,
