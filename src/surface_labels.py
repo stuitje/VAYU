bd271e2bee58542a3b1f9c3344d4663cc9a3c60d
Karen Stuitje <e.k.e.stuitje@student.rug.nl>
2026-06-22 16:04:58 +0200

    add surface labels file mirroring atmos labels

---
+++ src/surface_labels.py
@@ -0,0 +1,46 @@
+surface_labels = {
+
+    # Volcanic igneous rocks
+    "tephrite":              "Tephrite",
+    "phonolite":             "Phonolite",
+    "trachyte":              "Trachyte",
+    "trachy_basalt":         "Trachybasalt",
+    "trachy_andesite":       "Trachyandesite",
+    "andesite":              "Andesite",
+    "rhyolite":              "Rhyolite",
+    "basalt_large":          "Basalt (large grain)",
+    "basalt_small":          "Basalt (small grain)",
+    "basalt_glass":          "Basalt glass",
+    "lunar_marebasalt":      "Lunar mare basalt",
+    "tholeiitic_basalt":     "Tholeiitic basalt",
+
+    # Pyroclastic rocks
+    "basalt_tuff":           "Basalt tuff",
+
+    # Plutonic igneous rocks
+    "gabbro":                "Gabbro",
+    "diorite":               "Diorite",
+    "granite":               "Granite",
+    "norite":                "Norite",
+    "syenite":               "Syenite",
+
+    # Ultramafic rocks
+    "harzburgite":           "Harzburgite",
+    "lherzolite":            "Lherzolite",
+    "ultramafic":            "Ultramafic",
+
+    # Extraterrestrial samples
+    "mars_basalticshergottite": "Martian basaltic shergottite",
+    "mars_breccia":          "Martian breccia",
+    "lunar_anorthosite":     "Lunar anorthosite",
+
+    # Minerals
+    "hematite":              "Hematite",
+    "pyrite":                "Pyrite",
+    "albite_dust":           "Albite dust",
+    "magnesium_sulphate":    "Magnesium sulphate",
+
+    # Reference
+    "greybody":              "Greybody",
+
+}
